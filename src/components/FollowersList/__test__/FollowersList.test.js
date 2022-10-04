08ebfbd5b341abce277ba4608be3ae9e7ba0b1e6
dev-raj-1729 <dev.raj.r1729@gmail.com>
2022-10-04 22:51:09 +0530

    added tests for FollowersList

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,25 @@
+import { screen, render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+import FollowersList from "../FollowersList";
+
+const FollowersListWrapper = () => {
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
+
+describe("FollowersList", () => {
+  it("should render follower items", async () => {
+    render(<FollowersListWrapper />);
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
+  });
+  it("should contain all followers", async () => {
+    render(<FollowersListWrapper />);
+    const followerDivElements = await screen.findAllByTestId(/follower-item/);
+    expect(followerDivElements.length).toBe(5);
+  });
+});
